1bde1cf1992c06c70d8c6464f67bb05d1a4dced2
chethangowdakb <chethangowdakb@gmail.com>
2023-04-05 09:05:41 +0530

    added test

--- LearningAPIDemo/UnitTest1.cs
+++ LearningAPIDemo/UnitTest1.cs
@@ -1,10 +1,22 @@
-﻿namespace LearningAPIDemo;
+﻿using System.Net;
+using RestSharp;
+
+namespace LearningAPIDemo;
 
 [TestClass]
 public class UnitTest1
 {
+    private readonly RestClient client;
+
+    public UnitTest1()
+    {
+        client = new RestClient("https://jsonplaceholder.typicode.com");
+    }
     [TestMethod]
     public void TestMethod1()
     {
+        var request = new RestRequest("/posts", Method.Get);
+        var response = client.Execute(request);
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
     }
 }
